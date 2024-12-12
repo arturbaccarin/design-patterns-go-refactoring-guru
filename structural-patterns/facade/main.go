0c998f2d643bc37721c43511a29d8930f42eba17
arturbaccarin <baccarin.artur@gmail.com>
2024-12-11 22:30:34 -0300

    add facade url

--- structural-patterns/facade/main.go
+++ structural-patterns/facade/main.go
@@ -1,6 +1,8 @@
 package main
 
 /*
+https://refactoring.guru/design-patterns/facade
+
 Facade is a structural design pattern that provides a simplified interface to a library,
 a framework, or any other complex set of classes.
 
