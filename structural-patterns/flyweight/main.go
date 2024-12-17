085af1055500227d2c7fdbc58cd79128274fcc8b
arturbaccarin <baccarin.artur@gmail.com>
2024-12-17 18:27:30 -0300

    empty

--- structural-patterns/flyweight/main.go
+++ structural-patterns/flyweight/main.go
@@ -3,6 +3,8 @@ package main
 /*
 https://refactoring.guru/design-patterns/flyweight
 
+Commit of the day
+
 Flyweight is a structural design pattern that lets you fit more objects into the
 available amount of RAM by sharing common parts of state between multiple objects
 instead of keeping all of the data in each object.
