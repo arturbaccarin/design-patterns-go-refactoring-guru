f46859ea8f321a869d91dd28a72901482b11ca41
arturbaccarin <baccarin.artur@gmail.com>
2024-12-22 10:15:06 -0300

    add proxy

--- structural-patterns/proxy/main.go
+++ structural-patterns/proxy/main.go
@@ -1,7 +1,108 @@
 package main
 
+import "fmt"
+
 /*
 https://refactoring.guru/design-patterns/proxy
 
+Proxy is a structural design pattern that lets you provide a substitute or placeholder for another object.
+A proxy controls access to the original object, allowing you to perform something either before or after
+the request gets through to the original object.
 
+Why would you want to control access to an object? Here is an example:
+you have a massive object that consumes a vast amount of system resources.
+You need it from time to time, but not always.
+
+The Proxy pattern suggests that you create a new proxy class with the same interface as an original service object.
+Then you update your app so that it passes the proxy object to all of the original object’s clients.
+Upon receiving a request from a client, the proxy creates a real service object and delegates all the work to it.
+
+But what’s the benefit? If you need to execute something either before or after the primary logic of the class,
+the proxy lets you do this without changing that class. Since the proxy implements the same interface as the original class,
+it can be passed to any client that expects a real service object.
+
+Proxy is a structural design pattern that provides an object that acts as a substitute for a real service object used by a client.
+A proxy receives client requests, does some work (access control, caching, etc.) and then passes the request to a service object.
+
+A web server such as Nginx can act as a proxy for your application server:
+
+* It provides controlled access to your application server.
+* It can do rate limiting.
+* It can do request caching.
 */
+
+// Subject
+type server interface {
+	handleRequest(string, string) (int, string)
+}
+
+// Real subject
+type Application struct {
+}
+
+func (a *Application) handleRequest(url, method string) (int, string) {
+	if url == "/app/status" && method == "GET" {
+		return 200, "Ok"
+	}
+
+	if url == "/create/user" && method == "POST" {
+		return 201, "User Created"
+	}
+	return 404, "Not Ok"
+}
+
+// Proxy
+type Nginx struct {
+	application       *Application
+	maxAllowedRequest int
+	rateLimiter       map[string]int
+}
+
+func newNginxServer() *Nginx {
+	return &Nginx{
+		application:       &Application{},
+		maxAllowedRequest: 2,
+		rateLimiter:       make(map[string]int),
+	}
+}
+
+func (n *Nginx) handleRequest(url, method string) (int, string) {
+	allowed := n.checkRateLimiting(url)
+	if !allowed {
+		return 403, "Not Allowed"
+	}
+	return n.application.handleRequest(url, method)
+}
+
+func (n *Nginx) checkRateLimiting(url string) bool {
+	if n.rateLimiter[url] == 0 {
+		n.rateLimiter[url] = 1
+	}
+	if n.rateLimiter[url] > n.maxAllowedRequest {
+		return false
+	}
+	n.rateLimiter[url] = n.rateLimiter[url] + 1
+	return true
+}
+
+func main() {
+
+	nginxServer := newNginxServer()
+	appStatusURL := "/app/status"
+	createuserURL := "/create/user"
+
+	httpCode, body := nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(appStatusURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createuserURL, "POST")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+
+	httpCode, body = nginxServer.handleRequest(createuserURL, "GET")
+	fmt.Printf("\nUrl: %s\nHttpCode: %d\nBody: %s\n", appStatusURL, httpCode, body)
+}
