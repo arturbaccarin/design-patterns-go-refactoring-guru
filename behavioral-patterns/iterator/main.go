312e0fd627dbcc2b85a4b2bf943c9076c0c55999
arturbaccarin <baccarin.artur@gmail.com>
2025-01-04 11:11:12 -0300

    add iterator url

--- behavioral-patterns/iterator/main.go
+++ behavioral-patterns/iterator/main.go
@@ -1,6 +1,8 @@
 package main
 
 /*
+https://refactoring.guru/design-patterns/iterator
+
 Iterator is a behavioral design pattern that lets you traverse elements of
 a collection without exposing its underlying representation (list, stack, tree, etc.).
 
