bf5e7d1882a7a4bab123ecbb8e4a345cf0ea9de5
arturbaccarin <baccarin.artur@gmail.com>
2025-01-05 08:37:11 -0300

    finish iterator

--- behavioral-patterns/iterator/main.go
+++ behavioral-patterns/iterator/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /*
 https://refactoring.guru/design-patterns/iterator
 
@@ -31,4 +33,79 @@ All iterators must implement the same interface.
 
 If you need a special way to traverse a collection, you just create a new iterator class,
 without having to change the collection or the client.
+
+Iterator is a behavioral design pattern that allows sequential traversal through a
+complex data structure without exposing its internal details.
 */
+
+// Collection
+type Collection interface {
+	createIterator() Iterator
+}
+
+// Concrete Collection
+type UserCollection struct {
+	users []*User
+}
+
+func (u *UserCollection) createIterator() Iterator {
+	return &UserIterator{
+		users: u.users,
+	}
+}
+
+// Iterator
+type Iterator interface {
+	hasNext() bool
+	getNext() *User
+}
+
+// Concrete Iterator
+type UserIterator struct {
+	index int
+	users []*User
+}
+
+func (u *UserIterator) hasNext() bool {
+	if u.index < len(u.users) {
+		return true
+	}
+	return false
+
+}
+func (u *UserIterator) getNext() *User {
+	if u.hasNext() {
+		user := u.users[u.index]
+		u.index++
+		return user
+	}
+	return nil
+}
+
+type User struct {
+	name string
+	age  int
+}
+
+func main() {
+
+	user1 := &User{
+		name: "a",
+		age:  30,
+	}
+	user2 := &User{
+		name: "b",
+		age:  20,
+	}
+
+	userCollection := &UserCollection{
+		users: []*User{user1, user2},
+	}
+
+	iterator := userCollection.createIterator()
+
+	for iterator.hasNext() {
+		user := iterator.getNext()
+		fmt.Printf("User is %+v\n", user)
+	}
+}
