01bf6a1ca2daab40b7caa1592213efb9a7673d2b
arturbaccarin <baccarin.artur@gmail.com>
2025-01-14 08:22:15 -0300

    add strategy link

--- behavioral-patterns/strategy/main.go
+++ behavioral-patterns/strategy/main.go
@@ -1,6 +1,8 @@
 package main
 
 /*
+https://refactoring.guru/design-patterns/strategy
+
 Strategy is a behavioral design pattern that lets you define a family of algorithms,
 put each of them into a separate class, and make their objects interchangeable.
 
