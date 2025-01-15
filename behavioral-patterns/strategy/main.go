ea5e74a8116803040f300525b28f17b258157230
arturbaccarin <baccarin.artur@gmail.com>
2025-01-15 08:04:24 -0300

    finish strategy theory

--- behavioral-patterns/strategy/main.go
+++ behavioral-patterns/strategy/main.go
@@ -29,4 +29,23 @@ into separate classes called strategies.
 The original class, called context, must have a field for storing a
 reference to one of the strategies. The context delegates the work to
 a linked strategy object instead of executing it on its own.
+
+The context isn’t responsible for selecting an appropriate algorithm for the job.
+Instead, the client passes the desired strategy to the context. In fact, the context
+doesn’t know much about strategies. It works with all strategies through the same
+generic interface, which only exposes a single method for triggering the algorithm
+encapsulated within the selected strategy.
+
+This way the context becomes independent of concrete strategies, so you can add new
+algorithms or modify existing ones without changing the code of the context or other strategies.
+
+In our navigation app, each routing algorithm can be extracted to its own class with a single
+buildRoute method. The method accepts an origin and destination and returns a collection of the route’s checkpoints.
+
+the main navigator class doesn’t really care which algorithm is
+selected since its primary job is to render a set of checkpoints on the map.
+
+Imagine that you have to get to the airport. You can catch a bus, order a cab,
+or get on your bicycle. These are your transportation strategies. You can pick
+one of the strategies depending on factors such as budget or time constraints.
 */
