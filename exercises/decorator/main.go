d1c85cda7fd74177b807f2939e2658a384fc49f1
arturbaccarin <baccarin.artur@gmail.com>
2025-02-02 17:20:47 -0300

    starting decorator exercise

---
+++ exercises/decorator/main.go
@@ -0,0 +1,7 @@
+package main
+
+/*
+In this exercise, you will implement a decorator in Go that measures
+the execution time of any given function. The decorator will wrap around
+the function and output the time it took for the function to execute.
+*/
