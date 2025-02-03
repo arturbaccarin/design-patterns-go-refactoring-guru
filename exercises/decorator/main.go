55a628ad83069e4b490c58550513718cd6007595
arturbaccarin <baccarin.artur@gmail.com>
2025-02-03 07:04:48 -0300

    finish decorator exercise

--- exercises/decorator/main.go
+++ exercises/decorator/main.go
@@ -1,7 +1,56 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
 /*
 In this exercise, you will implement a decorator in Go that measures
 the execution time of any given function. The decorator will wrap around
 the function and output the time it took for the function to execute.
 */
+
+type Operation interface {
+	Execute() error
+}
+
+type AnyFunction interface {
+	Execute() error
+}
+
+type MyFunction struct {
+	Name string
+}
+
+func (f *MyFunction) Execute() error {
+	time.Sleep(1 * time.Second)
+	fmt.Println("Executing operation:", f.Name)
+	return nil
+}
+
+type OperationDecorator struct {
+	Operation AnyFunction
+}
+
+func (d *OperationDecorator) Execute() error {
+	start := time.Now()
+	err := d.Operation.Execute()
+	duration := time.Since(start)
+	fmt.Printf("Operation %s took %s to execute\n", d.Operation, duration)
+	return err
+}
+
+func Execute(operation Operation) error {
+	operation.Execute()
+	return nil
+}
+
+func main() {
+	operation := &MyFunction{Name: "My Operation"}
+
+	decoratedOperation := &OperationDecorator{Operation: operation}
+
+	// Execute(operation)
+	Execute(decoratedOperation)
+}
