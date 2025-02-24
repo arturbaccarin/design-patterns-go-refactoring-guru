39fcb7ec00c07b4eb64ca59dac5435ece0e057a5
arturbaccarin <baccarin.artur@gmail.com>
2025-02-24 05:17:00 -0300

    add iterator exercise

--- exercises/iterator/main.go
+++ exercises/iterator/main.go
@@ -1,8 +1,58 @@
 package main
 
-/*
-Implement an Iterator pattern in Go by creating a structure
-that allows sequential access to a collection of elements.
+import "fmt"
 
-commit of the day
+/*
+In this exercise, we'll implement the Iterator pattern for a collection of Books.
+We’ll create a BookCollection that holds a list of books and an iterator
+to traverse over them.
 */
+
+type Iterator interface {
+	hasNext() bool
+	getNext() *Book
+}
+
+type Book struct {
+	title  string
+	author string
+}
+
+type BookCollection struct {
+	books []*Book
+	index int
+}
+
+func (b *BookCollection) hasNext() bool {
+	if b.index < len(b.books) {
+		return true
+	}
+	return false
+}
+
+func (b *BookCollection) getNext() *Book {
+	if b.hasNext() {
+		book := b.books[b.index]
+		b.index++
+		return book
+	}
+	return nil
+}
+
+func main() {
+	books := []*Book{
+		{title: "The Great Gatsby", author: "F. Scott Fitzgerald"},
+		{title: "To Kill a Mockingbird", author: "Harper Lee"},
+		{title: "Pride and Prejudice", author: "Jane Austen"},
+	}
+
+	collection := &BookCollection{books: books}
+
+	var iterator Iterator
+	iterator = collection
+
+	for iterator.hasNext() {
+		book := iterator.getNext()
+		fmt.Printf("Title: %s, Author: %s\n", book.title, book.author)
+	}
+}
